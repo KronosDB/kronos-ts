fec5807043c8700c88c4171028bcbeed58131cda
Theo Emanuelsson <skurutheo@gmail.com>
2026-05-19 23:01:48 +0200

    chore: remove @kronos-ts/nestjs extension

--- packages/extensions/nestjs/src/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  KronosModule,
-  KRONOS_APPLICATION,
-  KRONOS_COMMAND_GATEWAY,
-  KRONOS_QUERY_GATEWAY,
-  KRONOS_APP,
-  type KronosModuleOptions,
-  type KronosModuleAsyncOptions,
-} from "./kronos-module.js"
